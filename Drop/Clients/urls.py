3a1065f34d0bfff66ab5d3b7ae42041585229d03
AIIM00773 <122451440+AIIM00773@users.noreply.github.com>
2026-05-04 20:21:29 +0300

    structural-reset

--- Drop/Clients/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import (
-    CartView,  
-)
-
-urlpatterns = [
-    path('cart/', CartView.as_view(), name='cart-detail'),
-
-]
--- Drop/Merchants/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import MerchantListView, MerchantDetailView, MerchantRegisterView
-
-urlpatterns = [
-    path('', MerchantListView.as_view(), name='merchant-list'),
-    path('register/', MerchantRegisterView.as_view(), name='merchant-register'),
-    path('<int:pk>/', MerchantDetailView.as_view(), name='merchant-detail'),
-  
-]
